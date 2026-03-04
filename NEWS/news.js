cb9097802267796954a7cc8f304c00298a28f7ca
Anthony <lolstijl@gmail.com>
2026-03-04 19:34:28 +0100

    release v8

--- NEWS/news.js
+++ NEWS/news.js
@@ -1,15 +1,41 @@
 const articles = document.querySelectorAll('.article');
 
 articles.forEach(article => {
-    let menu_open = false;
+  let menu_open = false;
+  const text = article.querySelector('.article_text')
+
   article.addEventListener('click', function() {
-    if (menu_open == false) {
-        article.style.maxHeight = "4000px"
+    let windowWith = window.innerWidth;
+    if (windowWith < 1024) {
+      //gsm formaat
+      if (menu_open == false) {
+        text.style.paddingBottom = "20px"
+        article.style.gridTemplateRows = `150px 1fr`
+        article.style.animationName = "bounceOpen"
         menu_open = true
-    } else {
-        article.style.maxHeight = ""
+      } else {
+        text.style.paddingBottom = ""
+        article.style.gridTemplateRows = ""
+        article.style.animationName = "bounceClose"
         menu_open = false
+      }
     }
+    else {
+      //desktop
+      if (menu_open == false) {
+        text.style.paddingBottom = "20px"
+        article.style.gridTemplateRows = `200px 1fr`
+        article.style.animationName = "bounceOpen"
+        menu_open = true
+      } else {
+        text.style.paddingBottom = ""
+        article.style.gridTemplateRows = ""
+        article.style.animationName = "bounceClose"
+        menu_open = false
+      }
+    }
+
+    
   });
 });
 
