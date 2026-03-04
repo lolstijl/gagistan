64b9211037c5579186ddeea1944fb5f971141a12
Anthony <lolstijl@gmail.com>
2026-03-04 20:54:14 +0100

    release v9

--- NEWS/news.js
+++ NEWS/news.js
@@ -8,26 +8,37 @@ articles.forEach(article => {
     let windowWith = window.innerWidth;
     if (windowWith < 1024) {
       //gsm formaat
-      if (menu_open == false) {
+      if (menu_open == false) { //expand menu
         text.style.paddingBottom = "20px"
         article.style.gridTemplateRows = `150px 1fr`
         article.style.animationName = "bounceOpen"
         menu_open = true
-      } else {
-        text.style.paddingBottom = ""
-        article.style.gridTemplateRows = ""
-        article.style.animationName = "bounceClose"
-        menu_open = false
+      } else { //collapse menu
+        let textHeight = text.offsetHeight;
+        
+        if (textHeight >= 700) { //checkt of hoogte meer dan 600px
+          text.style.paddingBottom = ""
+          article.style.gridTemplateRows = ""
+          article.style.animationName = "bounceCloseHeavy"
+          menu_open = false
+        }
+        else { //hoogte is kleiner dan 600
+          text.style.paddingBottom = ""
+          article.style.gridTemplateRows = ""
+          article.style.animationName = "bounceClose"
+          menu_open = false
+        }
+        
       }
     }
     else {
       //desktop
-      if (menu_open == false) {
+      if (menu_open == false) { //expand menu
         text.style.paddingBottom = "20px"
         article.style.gridTemplateRows = `200px 1fr`
         article.style.animationName = "bounceOpen"
         menu_open = true
-      } else {
+      } else { //collapse menu
         text.style.paddingBottom = ""
         article.style.gridTemplateRows = ""
         article.style.animationName = "bounceClose"
