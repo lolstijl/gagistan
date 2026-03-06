d27a23fcc687bf9b264d255ceeb27b1246a0a7ac
Anthony <lolstijl@gmail.com>
2026-03-06 11:28:47 +0100

    release v10.7

--- header.js
+++ header.js
@@ -6,24 +6,35 @@ const lijn1 = document.querySelector(".lijn1")
 const lijn2 = document.querySelector(".lijn2")
 const lijn3 = document.querySelector(".lijn3")
 const ul = nav.querySelector("ul")
+const a = ul.querySelector('li a')
 
 let menu_open = false;
 function menu() {
     if (menu_open == false) {
         nav.style.gridTemplateRows = "1fr"
+
         ul.style.paddingTop = "20px"
         ul.style.paddingBottom = "10px"
+        ul.style.opacity = "100%"
+        ul.style.translate = "0px 0px"
+
         lijn1.style.animation = "collapse1 1s forwards"
         lijn2.style.animation = "collapse2 1s forwards"
         lijn3.style.animation = "collapse3 1s forwards"
+
         menu_open = true
     } else {
         nav.style.gridTemplateRows = ""
+
         ul.style.paddingTop = "0px"
         ul.style.paddingBottom = "0px"
+        ul.style.opacity = ""
+        ul.style.translate = ""
+
         lijn1.style.animation = "expand1 1s forwards"
         lijn2.style.animation = "expand2 1s forwards"
         lijn3.style.animation = "expand3 1s forwards"
+
         menu_open = false
     }
 };
