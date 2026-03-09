bf8babd22b83d20fcd536ee22074529349319187
Anthony <lolstijl@gmail.com>
2026-03-09 20:58:55 +0100

    DELETED

--- locked_page/script.js
+++
@@ -1,9 +0,0 @@
-const form = document.getElementById('myform');
-
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const text = document.getElementById('userInput').value;
-
-    alert("Incorrect password");
-});
