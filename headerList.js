e96668ef6c30154396cb3ea324369d2b6a5f8297
Anthony <lolstijl@gmail.com>
2026-03-12 09:49:45 +0100

    release v11.1

--- headerList.js
+++ headerList.js
@@ -6,11 +6,6 @@ const linkNews = '<li><a href="../NEWS/news.html">Gagistan NEWS</a></li>'
 const linkFiles = '<li><a href="../gagafiles/index.html">De gaga files</a></li>'
 const linkQuotes = '<li><a href="../under_construction/index.html">Quotes</a></li>'
 
-// IMPORT DE LINKS
-const linkFiles = '<li><a href="../locked_page/index.html">De gaga files</a></li>'
-const linkQuotes = '<li><a href="../under_construction/index.html">Quotes</a></li>'
-
-ul2.insertAdjacentHTML('afterbegin', linkQuotes)
 ul2.insertAdjacentHTML('afterbegin', linkFiles)
 ul2.insertAdjacentHTML('afterbegin', linkNews)
 ul2.insertAdjacentHTML('afterbegin', linkHome)
