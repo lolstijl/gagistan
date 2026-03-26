35dfa03552627cc898b75baf8947994925c7cea4
Anthony <lolstijl@gmail.com>
2026-03-26 22:14:20 +0100

    release v13.1

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // This tells Vite that the site is hosted in a sub-folder, not the root
   base: '/gagistan/',
+  build: {
+    cssCodeSplit: true,
+  }
 });
